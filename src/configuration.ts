3e06765d15c602bc2b206da62d0bf7f3bcf1bb19
Yuri Burger <yuriburger@outlook.com>
2018-04-12 22:35:45 +0200

    Initial setup

---
+++ src/configuration.ts
@@ -0,0 +1,36 @@
+VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
+    WidgetHelpers.IncludeWidgetConfigurationStyles();
+    VSS.register("QualityGateWidget-Configuration", function () {
+        const $queryDropdown = $("#query-path");
+
+        return {
+            load: function (widgetSettings, widgetConfigurationContext) {
+                const settings = JSON.parse(widgetSettings.customSettings.data);
+                if (settings && settings.queryPath) {
+                    $queryDropdown.val(settings.queryPath);
+                }
+                $queryDropdown.on("change", function () {
+                    const customSettings = {
+                        data: JSON.stringify({
+                            queryPath: $queryDropdown.val()
+                        })
+                    };
+                    const eventName = WidgetHelpers.WidgetEvent.ConfigurationChange;
+                    const eventArgs = WidgetHelpers.WidgetEvent.Args(customSettings);
+                    widgetConfigurationContext.notify(eventName, eventArgs);
+                });
+
+                return WidgetHelpers.WidgetStatusHelper.Success();
+            },
+            onSave: function () {
+                const customSettings = {
+                    data: JSON.stringify({
+                        queryPath: $queryDropdown.val()
+                    })
+                };
+                return WidgetHelpers.WidgetConfigurationSave.Valid(customSettings);
+            }
+        };
+    });
+    VSS.notifyLoadSucceeded();
+});
